a648fe3ce3d2eda023076cce48eba2c2c0473ddd
Elfeky <elfe8y@gmail.com>
2024-10-12 15:15:45 +0300

    Equipment update

--- depi-project/Depi-Project/Models/Equipment.cs
+++ depi-project/Depi-Project/Models/Equipment.cs
@@ -4,12 +4,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 public class Equipment
 {
     [Key]
-    public int?Equipment_Id { get; set; }
-    
+    public int Equipment_Id { get; set; }
+    [Required]
     public string? Equipment_Name {get ;set;}
-
+    [Required]
     public string? Maintainance_Date {get ; set;}
-
+    [Required]
     public string ? Status {get ; set ;}
 }
 }
